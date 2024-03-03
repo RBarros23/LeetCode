d175a4438a460fc890fb6ce0b2dbf4bf39babb54
Rui <rui.barros23@gmail.com>
2024-03-03 12:48:50 +0000

    Incomplete resolution

--- 20_Valid_Parentheses/src/index.ts
+++ 20_Valid_Parentheses/src/index.ts
@@ -1,5 +1,20 @@
 function isValid(s: string): boolean {
-    let nMap = new Map<string, string>();
+    let nMap = new Map([
+        ["(", ")"],
+        ["{", "}"],
+        ["[", "]"]
+    ]);
     
-    return false;
+    for(let i = 0; i < s.length - 1; i += 2){
+        if(s[i+1] !== nMap.get(s[i])){
+            console.log(`S +1: ${s[i+1]}`);
+            console.log(`S: ${s[i]}`);
+            console.log(`Map: ${nMap.get(s[i])}`)
+            return false
+        };
+    }
+    
+    return true;
 };
+
+console.log(isValid("{[]}"));
